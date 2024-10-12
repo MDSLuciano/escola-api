2573c3d69726d9b3a9d6a9a5f0a82cac15dfb25f
Luciano Machado <lucianoomachados@gmail.com>
2024-10-12 19:17:20 -0300

    estruturando o projeto

---
+++ app.js
@@ -0,0 +1,21 @@
+import expresss from 'express';
+import homeRoutes from './src/routes/homeRoutes';
+
+class App{
+  constructor(){
+    this.app = expresss();
+    this.middlewares();
+    this.routes();
+  }
+
+  middlewares(){
+    this.app.use(expresss.urlencoded({ extended: true}));
+    this.app.use(expresss.json())
+  }
+
+  routes(){
+    this.app.use('/', homeRoutes)
+  }
+}
+
+export default new App().app;
