0d249dcd5760071b4c5fa5b91a4c7970492b0fe3
Luciano Machado <lucianoomachados@gmail.com>
2024-10-13 19:39:29 -0300

    CRUD Usuários

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -5,13 +5,78 @@ class UserController {
     try{
       const novoUser = await User.create(req.body)
       res.json(novoUser)
+
     } catch(e){
         res.status(400).json({
           errors: e.errors.map(erro => erro.message),//pega todos os erros e coloca em um objeto contendo um array de erros
-
         })
     }
+  }
 
+  // Index
+  async index(req, res) {
+    try{
+      const users = await User.findAll();
+      res.json(users)
+    }catch(e){
+      return res.json(null)
+    }
+  }
+  // Show
+  async show(req, res) {
+    try{
+      const user = await User.findByPk(req.params.id);//Isso procura a Primary Key (id)
+      res.json(user)
+    }catch(e){
+      return res.json(null)
+    }
+  }
+
+  // Update
+  async update(req, res) {
+    try{
+      if(!req.params.id){
+        return res.status(400).json({errors: ["ID não enviado"]})
+      }
+
+      const user = await User.findByPk(req.params.id);//Isso procura a Primary Key (id)
+
+      if(!user){
+        return res.status(400).json({
+          errors: ["Usuário não existe"]
+        })
+      }
+
+      const novosDados = await user.update(req.body)
+
+      res.json(novosDados)
+    }catch(e){
+      res.status(400).json({
+        errors: e.errors.map(erro => erro.message),//pega todos os erros e coloca em um objeto contendo um array de erros
+      })
+    }
+  }
+  // Delete
+  async delete(req, res) {
+    try{
+      if(!req.params.id){
+        return res.status(400).json({errors: ["ID não enviado"]})
+      }
+
+      const user = await User.findByPk(req.params.id);//Isso procura a Primary Key (id)
+
+      if(!user){
+        return res.status(400).json({
+          errors: ["Usuário não existe"]
+        })
+      }
+      await user.destroy()
+      res.json(user)
+    }catch(e){
+      res.status(400).json({
+        errors: e.errors.map(erro => erro.message),//pega todos os erros e coloca em um objeto contendo um array de erros
+      })
+    }
   }
 }
 
--- src/models/User.js
+++ src/models/User.js
@@ -46,7 +46,9 @@ export default class User extends Model {
 
     // Metodo para criptografar a senha antes de salvar.
     this.addHook('beforeSave', async (user) => {
+      if(user.password){
         user.password_hash = await bcrypt.hash(user.password, 8);
+      }
     });
 
     return this;
