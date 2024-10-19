d4e5ef5c12072cd3f69dd769164b7db80cb2ee21
Luciano Machado <lucianoomachados@gmail.com>
2024-10-19 15:05:50 -0300

    CRUD Alunos e colocando autenticação em Alunos

--- src/controllers/AlunoController.js
+++ src/controllers/AlunoController.js
@@ -5,6 +5,122 @@ class AlunoController {
     const alunos = await Aluno.findAll();
     res.json(alunos)
   }
+
+  async store(req, res){ 
+    try{
+      const aluno = await Aluno.create(req.body)
+
+      return res.json(aluno)
+
+    }catch(e){
+      return res.status(400).json({
+        errors: e.errors.map(err => err.message),
+      })
+    }
+
+
+   }
+  async show(req, res){
+    try{
+      const { id } = req.params;
+
+      if(!id){
+        return res.status(400).json({
+          errors: ["Missing ID"]
+        })
+      }
+
+      const aluno = await Aluno.findByPk(id)
+
+      if(!aluno) {
+        if(!id){
+          return res.status(400).json({
+            errors: ["Student does not exist"],
+          })
+        }
+      }
+
+      return res.json(aluno)
+
+    }catch(e){
+      return res.status(400).json({
+        errors: e.errors.map(err => err.message),
+      })
+    }
+    
+  }
+  async delete(req, res){
+    try{
+      const { id } = req.params;
+
+      if(!id){
+        return res.status(400).json({
+          errors: ["Missing ID"]
+        })
+      }
+
+      const aluno = await Aluno.findByPk(id)
+
+      if(!aluno) {
+        if(!id){
+          return res.status(400).json({
+            errors: ["Student does not exist"],
+          })
+        }
+      }
+
+      await aluno.destroy()
+
+      return res.json({ deleted: true })
+
+    }catch(e){
+      if (e.errors && Array.isArray(e.errors)) {
+        return res.status(400).json({
+          errors: e.errors.map((err) => err.message),
+        });
+      } else {
+        return res.status(400).json({
+          errors: [e.message],
+        });
+      }
+    }
+  }
+  async update(req, res){
+    try{
+      const { id } = req.params;
+
+      if(!id){
+        return res.status(400).json({
+          errors: ["Missing ID"]
+        })
+      }
+
+      const aluno = await Aluno.findByPk(id)
+
+      if(!aluno) {
+        if(!id){
+          return res.status(400).json({
+            errors: ["Student does not exist"],
+          })
+        }
+      }
+
+      const alunoAtualizado = await aluno.update(req.body)
+      return res.json(alunoAtualizado)
+
+    }catch(e){
+      if (e.errors && Array.isArray(e.errors)) {
+        return res.status(400).json({
+          errors: e.errors.map((err) => err.message),
+        });
+      } else {
+        return res.status(400).json({
+          errors: [e.message],
+        });
+      }
+    }
+  }
+
 }
 
 export default new AlunoController();
