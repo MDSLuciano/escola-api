3c11f756c69bbf55b84aadbd02fd85d492d4c134
Luciano Machado <lucianoomachados@gmail.com>
2024-10-19 18:28:19 -0300

    Tratando os arquivos enviados

--- src/config/multerConfig.js
+++ src/config/multerConfig.js
@@ -4,7 +4,16 @@ import { extname, resolve } from "path";//Extname pega a extensão do arquivo
 const randow = () => Math.floor(Math.random() * 10000 + 10000);
 
 export default{
+    fileFilter: (req, file, callback) => {
+        if (file.mimetype !== "image/png" || file.mimetype !== "image/jpg" || file.mimetype !== "image/jpeg") {
+            return callback(new multer.MulterError("Arquivo precisar ser PNG, JPG ou JPEG"));
+        }
+
+            return callback(null, true);
+        },
+
     storage: multer.diskStorage({
+        
         destination: (req, file, callback) => {
             callback(null, resolve(__dirname, "..", "..", "uploads"));//Primeiro parâmetro seria o erro, segundo seria a pasta de destino
         },
